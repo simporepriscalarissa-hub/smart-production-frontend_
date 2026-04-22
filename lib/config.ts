1070d4103481ec14a7b0b11a28f07b3e51690f5a
Larissa Prisca Simpore <simporepriscalarissa@gmail.com>
2026-04-22 01:10:48 +0100

    fix apiUrl config

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
   entreprise: 'Entreprise',
-  apiUrl: 'http://localhost:3001',
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
   iaUrl: 'https://priscalarissa-smart-production-ia.hf.space',
   unite: 'produits',
   references: ['Pièce-001', 'Pièce-002', 'Pièce-003'],
