310364ad1d5d63028d9f7f659b3949db1508db52
Larissa Prisca Simpore <simporepriscalarissa@gmail.com>
2026-04-22 01:32:15 +0100

    fix websocket transports

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,19 +1,16 @@
 import { io, Socket } from 'socket.io-client'
 import { APP_CONFIG } from './config'
 
-// On s'assure que l'URL est correcte (ex: http://localhost:3001)
-const SOCKET_URL = APP_CONFIG.apiUrl;
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || APP_CONFIG.apiUrl;
 
 export const socket: Socket = io(SOCKET_URL, {
-  // 'websocket' est plus stable pour le temps réel que 'polling'
-  transports: ['websocket'], 
+  transports: ['polling', 'websocket'],
   autoConnect: true,
   reconnection: true,
   reconnectionAttempts: 10,
   reconnectionDelay: 1000,
 });
 
-// Debugging pour voir l'état dans la console du navigateur (F12)
 socket.on('connect', () => {
   console.log('🚀 Connecté au serveur WebSocket :', socket.id);
 });
